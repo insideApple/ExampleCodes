48d1873203a4c16b5c60c40fb14fd7e0fa3098fa
Vivek Padaya <insideApple@users.noreply.github.com>
2019-03-09 00:37:48 +0530

    Add files via upload

--- Managers/APIManager.swift
+++ Managers/APIManager.swift
@@ -8,11 +8,10 @@
 
 import UIKit
 
-
-let kServerURLString = "https://itembi.iqdesk.co.il/api/api_public"
+//http://132.148.132.184:82/api/
+let kServerURLString = "https://globalapi.deliverload.com/api"
 typealias ErrorTuple = (statusCode: Int?, message: String)
 private let kUnknownError = "error"
-
 
 class APIManager {
     
@@ -45,99 +44,22 @@ class APIManager {
 
    
     
-    /**  General request method */
-    static func askQuestionWithQuestion(que : String, completion: @escaping RequestCompletion){
-//        let parameters: Parameters = ["uuid": UserManager.DeviceUDID ?? "" , "question" : que]
-//        let newRequestUrl = kServerURLString + "/question"
-//        request(newRequestUrl, method: .post, parameters: parameters, encoding: JSONEncoding.default)
-//            .downloadProgress(queue: DispatchQueue.global(qos: .utility)) { progress in
-//                print("Progress: \(progress.fractionCompleted)")
-//            }
-//            .validate { request, response, data in
-//                // Custom evaluation closure now includes data (allows you to parse data to dig out error messages if necessary)
-//                return .success
-//            }
-//            .responseJSON { response in
-//                debugPrint(response)
-//                if let result = response.result.value {
-//                    completion(.success(result: result as AnyObject))
-//
-//                    //                    completion(success(result: result))
-//                    return
-//                }
-//
-//
-//                print(response.description)
-//                print(response.result.value)
-//
-//
-//
-//                let data = response.data ?? NSData() as Data
-//
-//                let jsonObject : NSDictionary! = try? JSONSerialization.jsonObject(with: data, options: []) as! NSDictionary
-//
-//                var message: String?
-//
-//                if let string = jsonObject?["message"] as? String {
-//                    message = string
-//                } else if let error = response.result.error?.localizedDescription {
-//                    message = error
-//                }
-//
-//                let statusCode = response.response?.statusCode
-//
-//
-//
-//                let errorTuple = (statusCode, message ?? NSLocalizedString(kUnknownError, comment: ""))
-//                completion(.error(errorTuple: (statusCode: errorTuple.0, message: errorTuple.1)))
-//        }
-//    }
+  
+    static func testMethos(){
+        let headers: HTTPHeaders = [ "Accept": "application/json", "Content-Type": "application/json" ]
         
-        let parameters: Parameters = ["uuid": UserManager.DeviceUDID! , "question" : que]
-        let newRequestUrl = kServerURLString + "/question"
+        let url = "https://maps.googleapis.com/maps/api/distancematrix/json?&origins=surat&destinations=navsari&key=AIzaSyDvt_KiUCtdb1kPEw4E4Dt68EuiF8PosAg"
         
-     
-        request(newRequestUrl, method: .post, parameters: parameters, encoding: URLEncoding.default)
-            .downloadProgress(queue: DispatchQueue.global(qos: .utility)) { progress in
-                print("Progress: \(progress.fractionCompleted)")
-            }
-            .validate { request, response, data in
-                // Custom evaluation closure now includes data (allows you to parse data to dig out error messages if necessary)
-                return .success
-            }
-            .responseJSON{ response in
-                debugPrint(response)
-                if let result = response.result.value {
-                    completion(.success(result: result as AnyObject))
-                    
-                    //                    completion(success(result: result))
-                    return
-                }
-                
-                
-                print(response.description)
-                print(response.result.value)
-                
-                
-                
-                let data = response.data ?? NSData() as Data
-                
-                let jsonObject : NSDictionary! = try? JSONSerialization.jsonObject(with: data, options: []) as! NSDictionary
-                
-                var message: String?
-                
-                if let string = jsonObject?["message"] as? String {
-                    message = string
-                } else if let error = response.result.error?.localizedDescription {
-                    message = error
-                }
-                
-                let statusCode = response.response?.statusCode
-                
-                
-                
-                let errorTuple = (statusCode, message ?? NSLocalizedString(kUnknownError, comment: ""))
-                completion(.error(errorTuple: (statusCode: errorTuple.0, message: errorTuple.1)))
+        let header: HTTPHeaders = [ "Accept": "application/json", "Content-Type": "application/json" ]
+        
+        request( url, method: .get, encoding: JSONEncoding.default, headers : header) .responseString {  response in
+            
+            print(response.request)  // original URL request
+            print(response.response) // HTTP URL response
+            print(response.data)     // server data
+            print(response.result)
+            
+            
         }
     }
     
@@ -154,23 +76,38 @@ class APIManager {
                 return .success
             }
             .responseJSON{ response in
-                debugPrint(response)
+//                debugPrint(response)
                 if let result = response.result.value {
                     if let dict : NSDictionary = result as? NSDictionary {
-                        if let statusCodeResult : Int = dict["status_code"] as? Int {
-                            if statusCodeResult != 200{
-                                
-                                let statusMsg = VPLocalizationManager.string(key: "code_\(statusCodeResult)")
-                                let errorTuple = (statusCodeResult, statusMsg)
-                                completion(.error(errorTuple: (statusCode: errorTuple.0, message: errorTuple.1 )))
-                            }
-                        }
+//                        print(dict["status_code"]! as! NSNumber)
+                       
+//                       
+//                        if let statusCodeResultStr : String = "\(dict["status_code"])" {
+//                            
+//                            if let statusCodeResult : Int = Int(statusCodeResultStr.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()) {
+//                                if statusCodeResult != 200{
+//                                    
+//                                    if statusCodeResult == 300 {
+//                                        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//                                        appDelegate.logout()
+//                                    }
+//
+//                                    let statusMsg = "\(statusCodeResultStr)"
+//                                    let errorTuple = (Int(statusCodeResult), statusMsg)
+//                                    completion(.error(errorTuple: (statusCode: errorTuple.0, message: errorTuple.1 )))
+//                                    return
+//                                }
+//                            }
+//                        }
                     }
                     
                     completion(.success(result: result as AnyObject))
                     return
                 }
                 
+                
+//                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//                appDelegate.logout()
                 
                 let data = response.data ?? NSData() as Data
                 
@@ -192,67 +129,142 @@ class APIManager {
                 completion(.error(errorTuple: (statusCode: errorTuple.0, message: errorTuple.1)))
         }
     }
-    
-   static func fetchData(){
-        self.makeRequestWithMethod(methodName: "getData", HttpMethod: .get, params:Parameters()) { (response) in
-            print("Result data : \(response.result ?? "" as AnyObject)")
+
+    static func uploadImage(image :UIImage , fileName : String, methodName: String, HttpMethod : HTTPMethod, params: Parameters, completion: @escaping RequestCompletion){
+        let newRequestUrl = kServerURLString + "/\(methodName)"
+
+        guard let data = image.jpegData(compressionQuality: 0.9) else {
+            return
         }
-    }
-    
-    static func checkVersion(){
-        self.makeRequestWithMethod(methodName: "getVersion", HttpMethod: .get, params:Parameters()) { (response) in
-            print("Result Version : \(response.result ?? "" as AnyObject)")
-            if let responseDict : NSDictionary = response.result as? NSDictionary{
-                let data = responseDict["data"] as! NSDictionary
-                if let languageVersion = data["language_version"]{
-                    let languageVerInt = Int(languageVersion as! String)
-                    if  UserManager.LanguageVersion! != languageVerInt!{
-                        UserManager.LanguageVersion = languageVerInt
-                        self.downloadLanguage()
-                    }
-                }
+
+      
+        
+        upload(multipartFormData: { (multipartFormData) in
+            multipartFormData.append(data, withName: fileName, fileName: "\(randomString(length: 6)).jpg", mimeType: "image/jpeg")
+            for (key, value) in params {
+                multipartFormData.append(("\(value)").data(using: String.Encoding(rawValue: String.Encoding.utf8.rawValue))!, withName: key)
             }
-        }
-    }
+        }, to:newRequestUrl)
+        { (result) in
+            switch result {
+            case .success(let upload, _, _):
+                
+                upload.uploadProgress(closure: { (progress) in
+                    //Print progress
+                })
+                
+                upload.responseJSON { response in
+                    //print response.result
+                    debugPrint(response)
+                    if let result = response.result.value {
+                        if let dict : NSDictionary = result as? NSDictionary {
     
-    static func downloadLanguage(){
-        self.makeRequestWithMethod(methodName: "getData", HttpMethod: .get, params:Parameters()) { (response) in
-            print("Result data : \(response.result ?? "" as AnyObject)")
-            if let dict : NSDictionary = response.result as? NSDictionary{
-                if let dataArray : [NSDictionary] = dict["data"] as? [NSDictionary]{
-                    for datasDict : NSDictionary in dataArray{
-                        if datasDict["type"] as! String == "languages"{
-                            getLanguageFile(langArray: datasDict["datas"] as! [NSDictionary])
                         }
+                        
+                        completion(.success(result: result as AnyObject))
+                        return
                     }
+                    
+                    
+//                    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//                    appDelegate.logout()
+                    
+                    let data = response.data ?? NSData() as Data
+                    
+                    let jsonObject : NSDictionary! = try? JSONSerialization.jsonObject(with: data, options: []) as! NSDictionary
+                    
+                    var message: String?
+                    
+                    if let string = jsonObject?["message"] as? String {
+                        message = string
+                    } else if let error = response.result.error?.localizedDescription {
+                        message = error
+                    }
+                    
+                    let statusCode = response.response?.statusCode
+                    
+                    
+                    
+                    let errorTuple = (statusCode, message ?? NSLocalizedString(kUnknownError, comment: ""))
+                    completion(.error(errorTuple: (statusCode: errorTuple.0, message: errorTuple.1)))
                 }
+                
+            case .failure(let encodingError):
+                let errorTuple = (401, encodingError.localizedDescription)
+                completion(.error(errorTuple: (statusCode: errorTuple.0, message: errorTuple.1)))
+                break
+                //print encodingError.description
+                
+            }
+        }
+    }
+    static func uploadDoc(data :Data , fileName : String, methodName: String, HttpMethod : HTTPMethod, params: Parameters, completion: @escaping RequestCompletion){
+        let newRequestUrl = kServerURLString + "/\(methodName)"
+        
+
+        
+        upload(multipartFormData: { (multipartFormData) in
+            multipartFormData.append(data, withName: fileName, fileName: "\(randomString(length: 6)).pdf", mimeType: "image/pdf")
+            for (key, value) in params {
+                multipartFormData.append((value as AnyObject).data(using: String.Encoding.utf8.rawValue)!, withName: key)
+            }
+        }, to:newRequestUrl)
+        { (result) in
+            switch result {
+            case .success(let upload, _, _):
+                
+                upload.uploadProgress(closure: { (progress) in
+                    //Print progress
+                })
+                
+                upload.responseJSON { response in
+                    //print response.result
+                    debugPrint(response)
+                    if let result = response.result.value {
+                        if let dict : NSDictionary = result as? NSDictionary {
+                            
+                        }
+                        
+                        completion(.success(result: result as AnyObject))
+                        return
+                    }
+                    
+                    
+//                    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//                    appDelegate.logout()
+//                    
+                    let data = response.data ?? NSData() as Data
+                    
+                    let jsonObject : NSDictionary! = try? JSONSerialization.jsonObject(with: data, options: []) as! NSDictionary
+                    
+                    var message: String?
+                    
+                    if let string = jsonObject?["message"] as? String {
+                        message = string
+                    } else if let error = response.result.error?.localizedDescription {
+                        message = error
+                    }
+                    
+                    let statusCode = response.response?.statusCode
+                    
+                    
+                    
+                    let errorTuple = (statusCode, message ?? NSLocalizedString(kUnknownError, comment: ""))
+                    completion(.error(errorTuple: (statusCode: errorTuple.0, message: errorTuple.1)))
+                }
+                
+            case .failure(let encodingError):
+                let errorTuple = (401, encodingError.localizedDescription)
+                completion(.error(errorTuple: (statusCode: errorTuple.0, message: errorTuple.1)))
+                break
+                //print encodingError.description
+                
             }
         }
     }
     
-    static func getLanguageFile(langArray : [NSDictionary]){
+    static func refreshUserData(){
         
-        for dictLan : NSDictionary in langArray {
-            downloadLanguageFileWith(name: dictLan["language"] as! String, fileUrl: URL.init(string: dictLan["iphone_file_url"] as! String)!)
-        }
     }
-    
-    static func downloadLanguageFileWith(name : String , fileUrl : URL){
-        
-        let destination: DownloadRequest.DownloadFileDestination = { _, _ in
-            var documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-            documentsURL.appendPathComponent(name)
-            documentsURL.appendPathExtension("plist")
-            return (documentsURL, [.removePreviousFile])
-        }
-        
-        download(fileUrl, to: destination).responseData { response in
-            if let destinationUrl = response.destinationURL {
-                print(destinationUrl)
-            }
-        }
-    
-    }
-    
 
 }
--- Managers/UserManager.swift
+++ Managers/UserManager.swift
@@ -7,54 +7,117 @@
 //
 
 import UIKit
+private let kUserModel = "UserModel"
 
 private let kSearchHistory = "SearchHistory"
 private let kLanguageVersion = "LanguageVersion"
 private let kUserName = "UserName"
+private let kFName = "FirstName"
+private let kLName = "LastName"
+
 private let kCompanyName = "CompanyName"
 private let kUserPhone = "UserPhoneNumber"
 private let kUserEmail = "UserEmail"
 private let kAboutMe = "AboutMe"
+private let kAboutUsUrl = "AboutUsURL"
+private let kAboutUsText = "AboutUsText"
+
+private let kIsGoToHome = "isGoToHome"
+private let kIsExpert = "isExpert"
+private let kIsPushUpdated = "isPushUpdated"
+
+private let kIsRegister = "IsRegister"
+private let kDeviceToken = "DeviceToken"
+private let kCountryCode = "CountryCode"
+
+private let kGroupId = "GroupID"
+private let kToken = "Token"
+private let kDeviceUDID = "DeviceUDID"
+private let kDistance = "Distance"
+
+private let kIsVehicleOwner = "IsVehicleOwner"
+private let kIsLoadOwner = "IsLoadOwner"
 
 
 class UserManager {
 
-    static var searchHistory: NSMutableArray?{
+    static var loginUser : UserModel!{
+        set{
+            if newValue == nil {
+                UserDefaults.standard.removeObject(forKey:kUserModel)
+            }else{
+                UserDefaults.standard.set(try! PropertyListEncoder().encode(newValue), forKey: kUserModel)
+                UserDefaults.standard.synchronize()
+            }
+        }
         get{
-            
-            let tempArray: NSMutableArray = UserDefaults.standard.mutableArrayValue(forKey: kSearchHistory)
-            
-            print("search History \(tempArray.count > 0 ? tempArray : [])")
-//            return UserDefaults.standard.mutableArrayValue(forKey: kSearchHistory)
-            return tempArray.count > 0 ? tempArray : []
-
+            if let storedObject: Data = UserDefaults.standard.object(forKey: kUserModel) as? Data {
+                do{
+                    let storedUser: UserModel? = try PropertyListDecoder().decode(UserModel.self, from: storedObject)
+                    if storedUser == nil {
+                        return UserModel()
+                    }else{
+                        return storedUser
+                    }
+                }catch{
+                    return nil
+                }
+                
+            }
+            return nil
+        }
+    }
+    
+    
+    static var isVehicleOwner : Bool{
+        get {
+            let vehicleOwner = UserDefaults.standard.bool(forKey: kIsVehicleOwner)
+            return vehicleOwner
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: kIsVehicleOwner)
+            UserDefaults.standard.synchronize()
+        }        
+    }
+    static var isLoadOwner : Bool{
+        get {
+            let isLoadOwner = UserDefaults.standard.bool(forKey: kIsLoadOwner)
+            return isLoadOwner
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: kIsLoadOwner)
+            UserDefaults.standard.synchronize()
+        }
+    }
+    
+    static var distance : Int?{
+        get{
+            let distance_temp = UserDefaults.standard.integer(forKey: kDistance)
+            if distance_temp == 0 {
+                return 1000
+            }
+            return distance_temp
         }
         set{
-            UserDefaults.standard.set(newValue, forKey: kSearchHistory)
+            UserDefaults.standard.set(newValue, forKey: kDistance)
             UserDefaults.standard.synchronize()
         }
     }
     
 
     static var DeviceUDID : String?{
-        return UIDevice.current.identifierForVendor!.uuidString
-    }
-    
-    
-    
-    
-    static var LanguageVersion : Int?{
-        
+        get{
+            let udid = UserDefaults.standard.string(forKey: kDeviceUDID) ?? ""
+            return udid
+        }
         set{
-            UserDefaults.standard.set(newValue, forKey: kLanguageVersion)
+            UserDefaults.standard.set(newValue, forKey: kDeviceUDID)
             UserDefaults.standard.synchronize()
         }
-        get{
-            let language = UserDefaults.standard.integer(forKey: kLanguageVersion)
-            return language
-        }
+        //        return UIDevice.current.identifierForVendor!.uuidString
     }
     
+
     static var userName : String?{
         
         set{
@@ -67,17 +130,30 @@ class UserManager {
         }
     }
     
-    static var companyName : String?{
+    static var fName : String?{
         
         set{
-            UserDefaults.standard.set(newValue, forKey: kCompanyName)
+            UserDefaults.standard.set(newValue, forKey: kFName)
             UserDefaults.standard.synchronize()
         }
         get{
-            let company = UserDefaults.standard.string(forKey: kCompanyName) ?? "Iqdesk Ltd"
-            return company
+            let user = UserDefaults.standard.string(forKey: kFName) ?? ""
+            return user
         }
     }
+    
+    static var lName : String?{
+        
+        set{
+            UserDefaults.standard.set(newValue, forKey: kLName)
+            UserDefaults.standard.synchronize()
+        }
+        get{
+            let user = UserDefaults.standard.string(forKey: kLName) ?? ""
+            return user
+        }
+    }
+    
     
     static var userPhone : String?{
         
@@ -104,15 +180,64 @@ class UserManager {
         }
     }
     
-    static var aboutMe : String?{
+   
+    static var isGoToHome : Bool!{
         set{
-            UserDefaults.standard.set(newValue, forKey: kAboutMe)
+            UserDefaults.standard.set(newValue, forKey: kIsGoToHome)
             UserDefaults.standard.synchronize()
         }
         get{
-            let aboutme = UserDefaults.standard.string(forKey: kAboutMe) ?? ""
-            return aboutme
+            let isHome = UserDefaults.standard.bool(forKey: kIsGoToHome)
+            return isHome
         }
     }
     
+    
+    
+    static var isRegister : Bool!{
+        set{
+            UserDefaults.standard.set(newValue, forKey: kIsRegister)
+            UserDefaults.standard.synchronize()
+        }
+        get{
+            let is_register = UserDefaults.standard.bool(forKey: kIsRegister)
+            return is_register
+        }
+    }
+    
+   
+    static var deviceToken : String?{
+        set{
+            UserDefaults.standard.set(newValue, forKey: kDeviceToken)
+            UserDefaults.standard.synchronize()
+        }
+        get{
+            let deviceToken = UserDefaults.standard.string(forKey: kDeviceToken) ?? "#simulator"
+            return deviceToken
+        }
+    }
+    
+    static var countryCode : String?{
+        set{
+            UserDefaults.standard.set(newValue, forKey: kCountryCode)
+            UserDefaults.standard.synchronize()
+        }
+        get{
+            let countycode = UserDefaults.standard.string(forKey: kCountryCode) ?? ""
+            return countycode
+        }
+    }
+    
+    
+    static var token : String?{
+        set{
+            UserDefaults.standard.set(newValue, forKey: kToken)
+            UserDefaults.standard.synchronize()
+        }
+        get{
+            let groupid = UserDefaults.standard.string(forKey: kToken) ?? ""
+            return groupid
+        }
+    }
+   
 }
